a36053d0ad72d55a6ede813a4e3d6047f2854401
richard-doyle <rdoyle8971@gmail.com>
2014-10-15 12:26:52 +0100

    Adds database using entity framework code first and sqlexpress

---
+++ SportsStore/SportsStore.Data/Context/SportsStoreContext.cs
@@ -0,0 +1,22 @@
+﻿namespace SportsStore.Data.Context
+{
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+
+    using SportsStore.Data.Entities;
+
+    public class SportsStoreContext : DbContext
+    {
+        public SportsStoreContext()
+            : base("SportsStoreContext")
+        {
+        }
+
+        public IDbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
--- SportsStore/SportsStoreService/Controllers/ProductsController.cs
+++ SportsStore/SportsStoreService/Controllers/ProductsController.cs
@@ -1,23 +1,19 @@
 ﻿namespace SportsStoreService.Controllers
 {
-    using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Http;
     using System.Web.Http.Cors;
 
-    using SportsStore.Domain.Models;
+    using SportsStore.Data.Context;
 
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ProductsController : ApiController
     {
+        private readonly SportsStoreContext db = new SportsStoreContext();
+
         public IHttpActionResult GetAll()
         {
-            var products = new List<Product>
-            {
-                new Product { Name = "Football", Price = 5.0 },
-                new Product { Name = "Tennis Raquet", Price = 24.99 },
-                new Product { Name = "Golf Clubs", Price = 248.75 }
-            };
-            return this.Ok(products);
+            return this.Ok(this.db.Products.ToList());
         }
     }
 }
