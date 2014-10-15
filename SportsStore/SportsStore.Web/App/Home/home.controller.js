0293f50d7b15fa4a2a22ffce2563272f7c69303e
richard-doyle <rdoyle8971@gmail.com>
2014-10-15 11:27:30 +0100

    Improves code style of angular JavaScript

---
+++ SportsStore/SportsStore.Web/App/Home/home.controller.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+    
+    angular
+        .module('app')
+        .controller('Home', ['$http', Home]);
+
+    function Home($http) {
+        var vm = this;
+        vm.products = {};
+
+        $http.get('http://localhost:58321/api/products').success(function (data) {
+            vm.products = data;
+        });
+    }
+})();
